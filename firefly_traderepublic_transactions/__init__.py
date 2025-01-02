5bce51682fd9f5638ef03fab5e3d935f6d892732
Julio Benavides <47013466+jubepue@users.noreply.github.com>
2025-01-02 21:00:36 +0100

    Create __init__.py

---
+++ firefly_traderepublic_transactions/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'firefly_traderepublic_transactions'
+__description__ = 'Automatic import of Trade Republic transactions into FireflyIII personal finance manager'
+__version__ = '0.0.1'
+__author__ = 'Julio Benavides'
+__author_email__ = 'jubepue@gmail.com'
+__license__ = 'MIT'
+from .firefly_traderepublic_transactions import FireflyTraderepublicClient
